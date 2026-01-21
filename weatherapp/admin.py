f7e984ef2a9b384e24276ad18b20f5582592a966
kelvyn2012 <kudufo@gmail.com>
2026-01-21 12:43:39 +0100

    configured the admin page

--- weatherapp/admin.py
+++ weatherapp/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import City, WeatherCache
 
-# Register your models here.
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = ("name", "country", "user", "added_at")
+    list_filter = ("country", "added_at")
+    search_fields = ("name", "user__username")
+    ordering = ("-added_at",)
+
+
+@admin.register(WeatherCache)
+class WeatherCacheAdmin(admin.ModelAdmin):
+    list_display = ("city_name", "updated_at")
+    search_fields = ("city_name",)
+    ordering = ("-updated_at",)
+    readonly_fields = ("data", "forecast_data", "updated_at")
