fd1ded20709a33aa693971a649addd906a3630db
kelvyn2012 <kudufo@gmail.com>
2026-01-23 13:21:52 +0100

    updated my urls to fit in the new path for geolocation

--- weatherapp/urls.py
+++ weatherapp/urls.py
@@ -1,3 +1,4 @@
+# weather/urls.py
 from django.urls import path
 from . import views
 
@@ -9,4 +10,5 @@ urlpatterns = [
     path("register/", views.register_view, name="register"),
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
+    path("api/location-weather/", views.get_location_weather, name="location_weather"),
 ]
